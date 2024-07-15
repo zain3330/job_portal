bf61537d5183cb5e5034d37c754c619bc6be82c3
zain3330 <zainmushtaq055@gmail.com>
2024-07-15 15:07:14 +0000

    zain

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Frontend\HomeController;
+use \App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\JobsController;
 use App\Http\Controllers\Frontend\JobDetailController;
-use App\Http\Controllers\Admin\DashboardController;
-
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/',[HomeController::class,'index'])->name('index');
 
@@ -14,7 +16,3 @@ Route::get('/jobs',[JobsController::class,'index'])->name('jobs');
 
 
 Route::get('/Job-detail',[JobDetailController::class,'index'])->name('job-detail');
-Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
-    Route::get('/dashboard',[DashboardController::class,'index']);
-
-});
