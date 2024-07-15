49f7b81a514a26c0128da875b62f59bed2d9516a
zain3330 <zainmushtaq055@gmail.com>
2024-07-15 06:54:57 +0000

    4th commit

--- resources/views/admin/index2.blade.php
+++ resources/views/admin/index2.blade.php
@@ -1732,7 +1732,7 @@
 
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<!-- AdminLTE admin demo (This is only for demo purposes) -->
 <script src="dist/js/pages/dashboard2.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\JobsController;
 use App\Http\Controllers\Frontend\JobDetailController;
-//
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+use App\Http\Controllers\Admin\DashboardController;
+
 
 Route::get('/',[HomeController::class,'index'])->name('index');
 
@@ -16,3 +14,7 @@ Route::get('/jobs',[JobsController::class,'index'])->name('jobs');
 
 
 Route::get('/Job-detail',[JobDetailController::class,'index'])->name('job-detail');
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+    Route::get('/dashboard',[DashboardController::class,'index']);
+
+});
